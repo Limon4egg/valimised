e046c4f1b5bcef30ac29ef3af93d71f66c43455c
Limon4egg <76990925+Limon4egg@users.noreply.github.com>
2021-11-28 14:57:03 +0200

    Delete conf.php

--- conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-$kasutaja="daniilmihhailov20";
-$andmebaas="daniilmihhailov20";
-$parool="122345";
-$serverinimi="localhost";
-$yhendus=new mysqli($serverinimi, $kasutaja, $parool, $andmebaas);
-$yhendus->set_charset("utf8");
-?>
